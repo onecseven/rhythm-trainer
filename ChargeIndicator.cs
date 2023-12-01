9754b7148bdae4d1f6dced1331a863849f194871
Tati <iced.tati@gmail.com>
2023-12-01 19:21:36 -0300

    rename

--- ChargeIndicator.cs
+++ ChargeIndicator.cs
@@ -3,20 +3,11 @@ using System;
 
 public class ChargeIndicator : TextureRect
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
     public Texture green = GD.Load<Texture>("static/green.tres");
     public Texture red = GD.Load<Texture>("static/red.tres");
     public Texture yellow = GD.Load<Texture>("static/yellow.tres");
     public int current = 0;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
-
-    public void turn(InputReader.ChargingState next)
+    public void OnChargingStateChanged(InputReader.ChargingState next)
     {
         if (current == ((int)next)) return;
         switch (next)
